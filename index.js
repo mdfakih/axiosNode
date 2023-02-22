0329e849395aa8f4b949d5fa4869bbf76e9be618
mdfakih <mf9049@gmail.com>
2023-02-22 13:50:12 +0530

    new change

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const app = express()
 
-const PORT = process.env.PORT || 3001
-
 const bodyParser = require("body-parser")
 
 const axios = require("axios")
@@ -10,6 +8,7 @@ const axios = require("axios")
 const dotenv = require("dotenv")
 dotenv.config()
 
+const PORT = process.env.PORT || 3001
 
 app.use(bodyParser.json())
 
